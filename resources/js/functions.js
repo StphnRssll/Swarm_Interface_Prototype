4be69a56fcaaa08d51d09ca8e9faeb4c45d1c0c9
StphnRssll <root@Swift-SDR.localdomain>
2019-09-18 18:53:53 -0400

    Organized index

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -1,12 +1,10 @@
 
 function drawMagnet(){
     var magnet = document.getElementById("magnet");
-    magnet.style.position = "absolute"; //css
-    // calculate rotation angle of magnet
-    var angle = Math.atan2(puckPos.y - mousePos.y, puckPos.x - mousePos.x );
+    magnet.style.position = "absolute";
+    var angle = Math.atan2(puckPos.y - mousePos.y, puckPos.x - mousePos.x ); // calculate rotation angle of magnet
     angle = angle * (180/Math.PI);
-    // draw magnet
-    magnet.style.left = mousePos.x  + 15 + "px";
+    magnet.style.left = mousePos.x  + 15 + "px"; // draw magnet
     magnet.style.top = mousePos.y + 50 + "px";
     magnet.style.transform = "rotate(" + angle  + "deg)";
 }
@@ -14,21 +12,22 @@ function drawMagnet(){
 function drawPuck(){
     var puck = document.getElementById("puck");
     puck.style.position = "absolute";
-    // distance from center to puck
-    var centerDistX = puckPos.x - midX; 
+    var centerDistX = puckPos.x - midX; // distance from center to puck
     var centerDistY = puckPos.y - midY;
-    var centerDistScal = Math.sqrt(centerDistX**2 + centerDistY**2);
-    // distance from mouse to puck
-    var mouseDistX = mousePos.x - puckPos.x; 
+    var centerDistScal = Math.sqrt(centerDistX**2 + centerDistY**2); 
+    var mouseDistX = mousePos.x - puckPos.x; // distance from mouse to puck
     var mouseDistY = mousePos.y - puckPos.y;
 
+    
     //  iteratively change puck position based on centerDist and mouseDist
+    var puckDrag = 200; // defines "how slow" the puck follows the mouse 
     if(centerDistScal<shapeRadius){ // if puck is in-bounds
         puckPos.x += mouseDistX/puckDrag; // puck moves towards mouse
         puckPos.y += mouseDistY/puckDrag;
     } else { // if puck is out of bounds
-        puckPos.x -= centerDistX/puckDrag; // puckPos.x - miX returns the distance between the position and the middle
-        puckPos.y -= centerDistY/puckDrag;
+        puckPos.x -= centerDistX/puckDrag; // puck moves towards the center
+        puckPos.y -= centerDistY/puckDrag; 
+        // centerDist is used because its sign (+/-) indicates which direction the puck should move
     }
 
     // draw puck - 10 and 25 are offsets to properly center the image on the mouse
