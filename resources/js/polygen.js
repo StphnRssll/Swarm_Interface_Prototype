873f68e7d4ffdb223c7af2a2eaf40d685ee1cdc1
StphnRssll <root@Swift-SDR.localdomain>
2019-09-18 17:25:26 -0400

    Added game circle and rotating magnet

--- resources/js/polygen.js
+++ resources/js/polygen.js
@@ -18,10 +18,11 @@ function PolyGen(c,a,m,e,r,o,n,s) {
 	if (!n) n =  a.width / 2; // x
 	if (!s) s = a.height / 2; // y
 	
-	var xPos;
-	var yPos;
-	context.fillStyle = "#014421";
-	context.font = "bold 25px Arial";
+	// all commented code in this file is used to draw coordinates beside each point of shape
+	// var xPos;
+	// var yPos;
+	// context.fillStyle = "#014421";
+	// context.font = "bold 25px Arial";
 	m.beginPath();
 	m.moveTo(n +  e * Math.cos(0), s +  e *  Math.sin(0));          
 	for (i = 1; i <= c + 1; i++){
@@ -30,7 +31,7 @@ function PolyGen(c,a,m,e,r,o,n,s) {
 		m.lineTo(xPos,yPos);
 		xPos = Math.round(xPos * 10) / 10;
 		yPos = (Math.round(yPos * 10) / 10);
-		context.fillText("(" + xPos + "," + Math.round((a.height-yPos) * 10) / 10 + ")", xPos, ((yPos**1.25)/4.8));
+		// context.fillText("(" + xPos + "," + Math.round((a.height-yPos) * 10) / 10 + ")", xPos, ((yPos**1.25)/4.8));
 	}
 	m.stroke();
 }
