76cb492cb575d5de9a4970d0d150636eb53132e6
StphnRssll <steve@Swift-SDR.localdomain>
2019-10-04 17:35:56 -0400

    cleaning

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -19,7 +19,7 @@ function drawPuck(){
     var mouseDistY = mousePos.y - puckPos.y;
 
     // iteratively change puck position based on centerDist and mouseDist
-    var puckDrag = 300; // defines "how slow" the puck follows the mouse 
+    var puckDrag = 100; // defines "how slow" the puck follows the mouse 
     if(centerDistScal<shapeRadius){ // if puck is in-bounds
         puckPos.x += mouseDistX/puckDrag; // puck moves towards mouse
         puckPos.y += mouseDistY/puckDrag;
@@ -28,10 +28,10 @@ function drawPuck(){
         puckPos.y -= centerDistY/puckDrag; 
         // centerDist is used because its sign (+/-) indicates which direction the puck should move
     }
-
     // draw puck - 10 and 25 are offsets to properly center the image on the mouse
     puck.style.left = puckPos.x - 10 + "px";
-    puck.style.top = puckPos.y + 25 + "px";   
+    puck.style.top = puckPos.y + 25 + "px";
+
 }
 
 function drawPuckLine(){
@@ -42,7 +42,7 @@ function drawPuckLine(){
 
     var puckDrag = 150;
     puckPos.y = midY;
-    if(Math.abs(centerDistX)<shapeRadius){ 
+    if(Math.abs(centerDistX)<shapeRadius){
         puckPos.x += mouseDistX/puckDrag; 
     } else { // if puck is out of bounds
         puckPos.x -= centerDistX/puckDrag; // puck moves towards the center
@@ -51,7 +51,7 @@ function drawPuckLine(){
 
     // draw puck - 10 and 25 are offsets to properly center the image on the mouse
     puck.style.left = puckPos.x - 10 + "px";
-    puck.style.top = puckPos.y + 25 + "px";   
+    puck.style.top = puckPos.y + 25 + "px";  
 }
 
 function countDown(){
@@ -69,4 +69,42 @@ function countDown(){
         }
     }, 1000);
     context.clearRect(midX-30, midY-250,60,85);
+}
+
+function driver(canvas,context){
+
+        // when a menu option is selected
+        document.getElementById('gen-button').addEventListener("click", function(){ 
+            var sidesCount = document.getElementById('sidesmenu').value; // get menu selection
+            PolyGen(sidesCount, document.querySelector("canvas"), 0, shapeRadius, 5, "#222", 0, 0);
+            clearInterval(countDown);
+            countDown();
+            //recordData();
+            setInterval(drawMagnet,20);
+            context.clearRect(midX-30, midY-250,60,85);
+            if(sidesCount != 2){
+                clearInterval(puckLineInterval);
+                puckInterval = setInterval(drawPuck,20); // draw puck every 10 ms
+                // draw game circle
+                context.arc(midX, midY, shapeRadius, 0, 2 * Math.PI, false); 
+                context.lineWidth = 1;
+                context.stroke();
+            } else { puckLineInterval = setInterval(drawPuckLine,20); }
+            document.getElementById('magnet').style.visibility = 'visible';
+            document.getElementById('puck').style.visibility = 'visible';
+        });
+
+        // every time 'mousemove' event occurs, call getMousePos
+        var counter = 0;
+        canvas.addEventListener('mousemove', function(evt) {
+            mousePos = getMousePos(canvas, evt);
+            counter++
+            if(counter%100 == 0){
+                console.log(mousePos);
+            }
+        }, false);
+}
+
+function getMousePos(canvas, evt) {
+    return { x: evt.clientX - rect.left, y: evt.clientY - rect.top }
 }
